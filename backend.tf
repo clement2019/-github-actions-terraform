317fefba048905cbca2e77800b6d118deb4dbbdc
clement2019 <ayeniayokunle@yahoo.co.uk>
2024-10-02 22:21:30 +0100

    initial commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bobby"
-    region         = "us-east-1"
+    bucket         = "my-terraform-state-clement11"
+    region         = "eu-west-2"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
   }
